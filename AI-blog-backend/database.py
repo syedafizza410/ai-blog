5863625b4181f57dd5c99f8c9f55d1dfccfddabc
syeda ummefizza <162727504+syedafizza410@users.noreply.github.com>
2025-11-02 02:12:30 +0500

    add

--- AI-blog-backend/database.py
+++ AI-blog-backend/database.py
@@ -4,8 +4,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 metadata = MetaData()
